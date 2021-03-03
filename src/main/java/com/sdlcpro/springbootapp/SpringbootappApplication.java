368e2669fe340bf799570c0392aff6720166163a
tanvir-tech <tanvirah325@gmail.com>
2021-03-03 20:42:49 +0600

    Initial commit -> Spring Boot application

---
+++ src/main/java/com/sdlcpro/springbootapp/SpringbootappApplication.java
@@ -0,0 +1,15 @@
+package com.sdlcpro.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootappApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootappApplication.class, args);
+
+    }
+
+}
